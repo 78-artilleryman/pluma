43a4ee079f15c43742729eaaacb090614994d42f
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-11-30 11:28:43 +0900

    Feat : Loading Spinner
    로그인 로딩 시 스피너 추가

--- src/components/Auth/KakaoRedirectHandler.tsx
+++ src/components/Auth/KakaoRedirectHandler.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { kakaoLoginRequest } from "../../store/auth/authActions"; // 가정된 액션
-
+import { ClimbingBoxLoader } from "react-spinners";
+import { getTheme } from "src/store/theme/themeSelectors";
 const KakaoRedirectHandler: React.FC = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
+  const theme = useSelector(getTheme);
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
@@ -18,7 +19,20 @@ const KakaoRedirectHandler: React.FC = () => {
     }
   }, [dispatch, navigate]);
 
-  return <div>Processing Kakao Login...</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100vh", // 화면 전체 높이를 차지하도록 설정합니다.
+      }}
+    >
+      <h2 style={{ margin: "1rem" }}>카카오 로그인 시도 중</h2>
+      <ClimbingBoxLoader size={30} color={theme === "dark" ? "#ffffff" : "#000000"} />
+    </div>
+  );
 };
 
 export default KakaoRedirectHandler;
